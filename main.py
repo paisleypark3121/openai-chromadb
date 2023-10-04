8a35df8952176118da3c21afca4ffb162d6b79fd
paisleypark <paisleypark@outlook.it>
2023-10-04 12:06:43 +0200

    added functionalities to the manager

--- main.py
+++ main.py
@@ -1,66 +1,216 @@
 import os
 from dotenv import load_dotenv
 
-from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import TextLoader
 
-def vectordb_exists(persist_directory):
-    return os.path.exists(persist_directory)
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 
-def create_vectordb(filename,persist_directory,embedding):
-    if vectordb_exists(persist_directory)==False:
-        print("creating vectordb")
-        loader = TextLoader(filename,encoding="utf-8")
-        documents = loader.load()
+from chromadb_manager import *
 
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        texts = text_splitter.split_documents(documents)
 
-        # persist to vectordb: in a notebook, we should call persist() to ensure the embeddings are written to disk
-        # This isn't necessary in a script: the database will be automatically persisted when the client object is destroyed
-        return Chroma.from_documents(
-            documents=texts, 
-            embedding=embedding, 
-            persist_directory=persist_directory
-        )
-    else:
-        return load_vectordb(persist_directory=persist_directory,embedding=embedding)
+def simple_query_from_file():
+    '''
+    Performs the query using the basic method
+    '''
+    filename="./files/state_of_the_union.txt"
+    persist_directory = 'chroma'
+    embedding = OpenAIEmbeddings()
+    overwrite=True
 
-def load_vectordb(persist_directory,embedding):
-    if vectordb_exists(persist_directory):
-        print("vectordb already exists")
-        return Chroma(persist_directory=persist_directory, embedding_function=embedding)
-    else:
-        raise ValueError(f"VectorDB does not exist in {persist_directory}")
+    vectordb=create_vectordb_from_file(
+        filename=filename,
+        persist_directory=persist_directory,
+        embedding=embedding,
+        overwrite=overwrite)
 
-def do_query(vectordb,llm,query):
-    qa = RetrievalQA.from_chain_type(
-        llm=llm, 
-        chain_type="stuff", 
-        retriever=vectordb.as_retriever()
-    )    
-    return qa.run(query)
+    llm=OpenAI()
+    query = "What did the president say about Ketanji Brown Jackson"
+
+    response=do_query(
+        vectordb=vectordb,
+        llm=llm,
+        query=query)
+    print(response)
+
+def chain_query_from_file():
+    '''
+    Performs the query using the chain method
+    '''
+    filename="./files/state_of_the_union.txt"
+    persist_directory = 'chroma'
+    embedding = OpenAIEmbeddings()
+    overwrite=True
+    
+    vectordb=create_vectordb_from_file(
+        filename=filename,
+        persist_directory=persist_directory,
+        embedding=embedding,
+        overwrite=overwrite)
+
+    llm=OpenAI()
+
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        {"context": vectordb.as_retriever(), "question": RunnablePassthrough()} 
+        | prompt 
+        | llm 
+        | StrOutputParser()
+    )
+
+    query = "What did the president say about Ketanji Brown Jackson"
+    response=chain.invoke(query)
+    print(response)
+
+def simple_query_from_youtube():
+    url="https://www.youtube.com/watch?v=KjOUQBzl2Ug&list=PLc53cT1vE-OLHeGIFHwLvMqNqRm4BH6Jw"
+    language_code="it"
+    local_file=save_remote_file(
+        url=url,
+        language_code=language_code
+    )
+    
+    persist_directory = 'chroma'
+    embedding = OpenAIEmbeddings()
+    overwrite=True
+    vectordb=create_vectordb_from_file(
+        filename=local_file,
+        persist_directory=persist_directory,
+        embedding=embedding,
+        overwrite=True
+    )
+
+    llm=OpenAI()
+    query = "What is an Access List?"
+
+    response=do_query(
+        vectordb=vectordb,
+        llm=llm,
+        query=query)
+    print(response)
+
+def simple_query_from_texts():
+    persist_directory = 'chroma'
+    embedding = OpenAIEmbeddings()
+    overwrite=True
+
+    texts=[
+        "Prince is a great musician", 
+        "He is a multi-instrumentalist", 
+        "We produced, arranged, composed and performed most of his albums", 
+        "My favorite album is The Gold Experience", 
+        "He was born in 1958"]
+
+    vectordb=create_vectordb_from_texts(
+        texts=texts,
+        persist_directory=persist_directory,
+        embedding=embedding,
+        overwrite=overwrite)
+
+    llm=OpenAI()
+    query = "When did Prince was born?"
+
+    response=do_query(
+        vectordb=vectordb,
+        llm=llm,
+        query=query)
+    print(response)
+
+def chain_query_from_texts():
+    persist_directory = 'chroma'
+    embedding = OpenAIEmbeddings()
+    overwrite=True
+
+    texts=[
+        "Prince is a great musician", 
+        "He is a multi-instrumentalist", 
+        "We produced, arranged, composed and performed most of his albums", 
+        "My favorite album is The Gold Experience", 
+        "He was born in 1958"]
+
+    vectordb=create_vectordb_from_texts(
+        texts=texts,
+        persist_directory=persist_directory,
+        embedding=embedding,
+        overwrite=overwrite)
+
+    llm=OpenAI()
+
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        {"context": vectordb.as_retriever(), "question": RunnablePassthrough()} 
+        | prompt 
+        | llm 
+        | StrOutputParser()
+    )
+
+    query = "When did Prince was born?"
+    response=chain.invoke(query)
+    print(response)
+
+def multiple_files():
+    urls = [
+        "https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/PT719-Transcript.pdf", 
+        "https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/PT717-Transcript.pdf",
+        "https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/PT715-Transcript.pdf",
+        "https://sds-platform-private.s3-us-east-2.amazonaws.com/uploads/PT713-Transcript.pdf"
+    ]
+    downloaded_files = []
+    for url in urls:
+        local_file = save_remote_file(url)
+        downloaded_files.append(local_file)
+    
+    persist_directory = 'chroma'
+    embedding = OpenAIEmbeddings()
+    overwrite=True
+    
+    vectordb=create_vectordb_from_files(
+        files=downloaded_files, 
+        persist_directory=persist_directory, 
+        embedding=embedding,
+        overwrite=overwrite
+    )
+
+    llm=OpenAI()
+
+    template = """Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        {"context": vectordb.as_retriever(), "question": RunnablePassthrough()} 
+        | prompt 
+        | llm 
+        | StrOutputParser()
+    )
+
+    query = "What is ToolFormer?"
+    response=chain.invoke(query)
+    print(response)
+
 
 load_dotenv()
 
-filename="state_of_the_union.txt"
-persist_directory = 'chroma'
-embedding = OpenAIEmbeddings()
-
-vectordb=create_vectordb(
-    filename=filename,
-    persist_directory=persist_directory,
-    embedding=embedding)
-
-llm=OpenAI()
-query = "What did the president say about Ketanji Brown Jackson"
-
-response=do_query(
-    vectordb=vectordb,
-    llm=llm,
-    query=query)
-print(response)
+#chain_query_from_text_file()
+#simple_query_from_texts()
+#chain_query_from_texts()
+#local_file=save_remote_file("https://www.gutenberg.org/cache/epub/1934/pg1934.txt")
+#simple_query_from_youtube()
+# multiple_files()
